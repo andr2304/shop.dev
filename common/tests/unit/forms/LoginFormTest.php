76d4f8973cacbf3580b2edee59b8e2c69050f3bf
ElisDN <mail@elisdn.ru>
2017-05-11 16:13:28 +0300

    Separated_entities_and_forms

--- common/tests/unit/forms/LoginFormTest.php
+++ common/tests/unit/forms/LoginFormTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace common\tests\unit\models;
+namespace common\tests\unit\forms;
 
 use Yii;
-use common\models\LoginForm;
-use common\fixtures\UserFixture;
+use common\forms\LoginForm;
+use common\fixtures\UserFixture as UserFixture;
 
 /**
  * Login form test
